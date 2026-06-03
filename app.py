37e4f4657c24b5bc3fa07849efba17c42224284c
Reethisraj <reethisraj@gmail.com>
2026-06-03 16:04:28 +0530

    Refactor analyze function and improve error handling

--- app.py
+++ app.py
@@ -9,35 +9,28 @@ app = Flask(__name__)
 CORS(app)
 
 UPLOAD_FOLDER = "uploads"
-
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@app.route("/")
+def home():
+    return "Smart Paper Tracer Backend is Running 🚀"
 
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
     if "answer_key" not in request.files:
-        return jsonify({
-            "error":"answer_key missing"
-        }),400
+        return jsonify({"error": "answer_key missing"}), 400
 
     if "student_sheet" not in request.files:
-        return jsonify({
-            "error":"student_sheet missing"
-        }),400
+        return jsonify({"error": "student_sheet missing"}), 400
 
     answer_key_file = request.files["answer_key"]
     student_file = request.files["student_sheet"]
 
-    answer_path = os.path.join(
-        UPLOAD_FOLDER,
-        answer_key_file.filename
-    )
-
-    student_path = os.path.join(
-        UPLOAD_FOLDER,
-        student_file.filename
-    )
+    answer_path = os.path.join(UPLOAD_FOLDER, answer_key_file.filename)
+    student_path = os.path.join(UPLOAD_FOLDER, student_file.filename)
 
     answer_key_file.save(answer_path)
     student_file.save(student_path)
@@ -45,15 +38,10 @@ def analyze():
     answer_text = extract_text(answer_path)
     student_text = extract_text(student_path)
 
-  
-
-    result = compare_answers(
-        answer_text,
-        student_text
-    )
+    result = compare_answers(answer_text, student_text)
 
     return jsonify(result)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
